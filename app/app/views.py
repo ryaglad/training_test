0be74d282ccdbf8840032371c6e297afe652a3cb
Ryan Gladwell <ryansgladwell@gmail.com>
2019-07-17 12:40:42 -0600

    added flask file upload

---
+++ app/app/views.py
@@ -0,0 +1,59 @@
+from app import app
+from flask import render_template
+from flask import request, redirect
+from flask import jsonify, make_response
+import os
+from werkzeug.utils import secure_filename
+
+
+
+
+app.config["IMAGE_UPLOADS"] = "/Users/sgladwe/Desktop/flask/"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
+
+def allowed_image(filename):
+
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+
+    if request.method == "POST":
+
+        if request.files:
+
+            image = request.files["image"]
+
+            if image.filename == "":
+                print("No filename")
+                return redirect(request.url)
+
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+
+                print("Image saved")
+
+                return redirect(request.url)
+
+            else:
+                print("That file extension is not allowed")
+                return redirect(request.url)
+
+    return render_template("public/upload_image.html")
+
+
+@app.route("/")
+def index():
+    print("Home Page")
+
+    return render_template("public/index.html")
